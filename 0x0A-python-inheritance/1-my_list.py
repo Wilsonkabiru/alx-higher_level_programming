52468e125754d478d2f0420e61d8278063a03788
Wilsonkabiru <wkabiru19@gmail.com>
2023-01-08 22:21:21 -0800

    all tasks

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module inherits from the list class"""
+
+
+class MyList(list):
+    """A class that inherits from list"""
+    def print_sorted(self):
+        """prints a sorted list"""
+        print(sorted(self))
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+
